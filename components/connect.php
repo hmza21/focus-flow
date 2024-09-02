02ed2c45a9a16f2a11a6bcf67ea00f359c33b181
Hamza Khattab <hamzakhattab@proton.me>
2024-09-02 19:21:29 +0300

    Database fix

--- components/connect.php
+++ components/connect.php
@@ -1,13 +1,13 @@
 <?php
 
-// $host = 'sql100.infinityfree.com';
-// $db = 'if0_37197927_focusflow';
-// $user = 'if0_37197927';
-// $pass = 'hNcC5h7vxJUdX';
+$host = 'sql100.infinityfree.com';
+$db = 'if0_37197927_focusflow';
+$user = 'if0_37197927';
+$pass = 'hNcC5h7vxJUdX';
 
-$host = 'localhost';
-$db = 'focusflow';
-$user = 'hamza';
-$pass = '0619';
+// $host = 'localhost';
+// $db = 'focusflow';
+// $user = 'root';
+// $pass = '';
 
 $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
