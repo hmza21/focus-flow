2d04daedce21f32869b503caa4759881860443ac
Hamza Khattab <hamzakhattab@proton.me>
2024-09-02 19:27:52 +0300

    Database fix and minify new CSS

--- components/connect.php
+++ components/connect.php
@@ -1,10 +1,12 @@
 <?php
 
-$host = 'sql100.infinityfree.com';
-$db = 'if0_37197927_focusflow';
-$user = 'if0_37197927';
-$pass = 'hNcC5h7vxJUdX';
+# Server credentials
+$host = 'localhost';
+$db = 'focusflow';
+$user = 'hamza';
+$pass = '0619';
 
+# XAMPP localhost credentials
 // $host = 'localhost';
 // $db = 'focusflow';
 // $user = 'root';
